52e26c47ec813837ae985274be5304bf5f36aa10
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2018-08-07 09:35:26 +0200

    Fixes for init commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux'; 
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import App from './app';
+import GameComponent from './layout/gameComponent'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducers,
+    applyMiddleware(ReduxThunk, logger , promiseMiddleware())
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/game" component={GameComponent}/>
+                <Route path="/" component={App}/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/layout/gameComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class GameComponent extends React.Component {
+
+}
+
+export default GameComponent;
