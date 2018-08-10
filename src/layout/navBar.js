ec35e9d832ec0410c8fed6007f28dd053e332e72
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2018-08-10 10:31:57 +0200

    Fix

--- src/layout/navBar.js
+++ src/layout/navBar.js
@@ -4,11 +4,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Image from 'material-ui-image'
 
 const styles = {
   root: {
