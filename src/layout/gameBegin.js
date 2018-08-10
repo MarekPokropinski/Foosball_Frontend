c22ea2a34e53df07e74c66d6fc0550cf1f7eed88
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-08-10 10:50:19 +0200

    fix

--- src/layout/gameBegin.js
+++ src/layout/gameBegin.js
@@ -32,7 +32,7 @@ class GameBegin extends Component {
     }
 
     connect() {
-        this.props.actions.connect('ws://' + ip + ':8080/matchInfo', (e) => {
+        this.props.actions.connect(`ws://${process.env.REACT_APP_HOST}/matchInfo`, (e) => {
             this.startGame();
         }, (e) => {
             this.props.actions.socketEvent(e.data);
