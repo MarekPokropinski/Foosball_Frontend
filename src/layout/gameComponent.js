ceee3ee670ced40356d650e45fdd8a814a0de4eb
Kacper Pomorski <relik1823@gmail.com>
2018-08-16 14:57:48 +0200

    colors

--- src/layout/gameComponent.js
+++ src/layout/gameComponent.js
@@ -9,30 +9,29 @@ import Time from '../time';
 import '../styles/gameStyle.css'
 
 class GameComponent extends React.Component {
-    componentDidMount() {
-        if(!this.props.user.socket) {
-            this.props.history.replace(`/begin/${this.props.match.params.mode}`)
-        } else {
-            this.props.actions.startTimer(1000, () => this.props.actions.timeStamp(1000))
-            this.props.actions.listenToSocket(this)
-        }
+   componentDidMount() {
+        this.props.actions.startTimer(1000, () => this.props.actions.timeStamp(1000))
+        this.props.actions.listenToSocket(this)
     }
 
     onMessage(e) {
+        console.log(e)
         if (this.props.user.gameState.finished) {
             this.handleGameFinish()
         }
     }
 
     waitAndShowSummary() {
-        setTimeout(() => this.props.history.replace("/summary"), 300);
+        setTimeout(() => {
+            this.props.history.replace("/summary")
+        }, 500);
     }
 
     handleGameFinish() {
         this.props.actions.stopTimer()
         this.props.actions.stopListeningToSocket(this)
-        //gets statistics from server and tells server that game is finished
-        this.props.actions.getStats(this.props.user.gameState.id, this.props.match.params.mode).then(() => this.waitAndShowSummary()); 
+        console.log(this.props.user.gameState.id)
+        this.props.actions.getStats(this.props.user.gameState.id).then(() => this.waitAndShowSummary()); //gets stats from server and finishes game
     }
 
     handleExitButton() {
@@ -45,11 +44,11 @@ class GameComponent extends React.Component {
     }
 
     handleRedIncrement() {
-        this.props.actions.incrementScore('RED', this.props.user.gameState.id, this.props.match.params.mode)
+        this.props.actions.incrementScore('RED', this.props.user.gameState.id)
     }
 
     handleBlueIncrement() {
-        this.props.actions.incrementScore('BLUE', this.props.user.gameState.id, this.props.match.params.mode)
+        this.props.actions.incrementScore('BLUE', this.props.user.gameState.id)
     }
 
     showNicks() {
@@ -75,7 +74,7 @@ class GameComponent extends React.Component {
 
     render() {
         let nicks = "";
-        if (this.props.match.params.mode !== 'free')
+        if (this.props.user.gameType !== 'free')
             nicks = this.showNicks();
 
         return (
@@ -83,15 +82,17 @@ class GameComponent extends React.Component {
                 <div className='timer'>
                     {this.getConvertedTime()}
                 </div>
+
                 <div >
                     {nicks}
                 </div>
+
                 <div className='score'>
-                    <p onClick={() => { this.handleRedIncrement() }}>{this.props.user.gameState.redScore}</p>
+                    <p className='redScore' onClick={() => { this.handleRedIncrement() }}>{this.props.user.gameState.redScore}</p>
                     <p>&nbsp;:&nbsp;</p>
-                    <p onClick={() => { this.handleBlueIncrement() }}>{this.props.user.gameState.blueScore}</p>
+                    <p className='blueScore' onClick={() => { this.handleBlueIncrement() }}>{this.props.user.gameState.blueScore}</p>
                 </div>
-                <Button className='finishButton' variant="contained" onClick={() => this.handleExitButton()}> Exit game </Button>
+                <Button className='finishButton' variant="outlined" onClick={() => this.handleExitButton()}> Exit game </Button>
             </div>
         );
     }
